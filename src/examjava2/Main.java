d518d1b688b8aa69c2ca487919c1a6ef6739794d
Cuongtt1121 <118425956+Cuongtt1121@users.noreply.github.com>
2023-06-22 16:27:45 +0700

    agian

--- src/examjava2/Main.java
+++ src/examjava2/Main.java
@@ -7,42 +7,62 @@ import java.util.Scanner;
 public class Main {
     static Scanner sc = new Scanner(System.in);
     private static Map<String, Customer> customerMap = new HashMap<>();
-    int choice;
+
+    public static void main(String[] args) {
+
+
+        int choice;
         do {
-        System.out.println("-------------List Customer------------ ");
-        System.out.println("1.Add new customer.");
-        System.out.println("2.Find by name");
-        System.out.println("3.Display all cutomer");
-        System.out.println("4.Exit");
-        System.out.print("Enter your option:... ");
-        choice = sc.nextInt();
-        sc.nextLine();
+            System.out.println("-------------List Customer------------ ");
+            System.out.println("1.Add new customer.");
+            System.out.println("2.Find by name");
+            System.out.println("3.Display all cutomer");
+            System.out.println("4.Exit");
+            System.out.print("Enter your option:... ");
+            choice = sc.nextInt();
+            sc.nextLine();
 
-        switch (choice) {
-            case 1:
-                displayAllCustomers();
-                break;
-            case 2:
-                findCustomerByName(sc);
-                break;
-            case 3:
-                displayAllCustomers(sc);
-                break;
-            case 4:
-                System.out.println("Ứng dụng đã kết thúc.");
-                break;
-            default:
-                System.out.println("Lựa chọn không hợp lệ. Vui lòng chọn lại.");
-                break;
+            switch (choice) {
+                case 1:
+                    AddCustomer();
+                    break;
+                case 2:
+                    findCustomerByName();
+                    break;
+                case 3:
+                    displayAllCustomers();
+                    break;
+                case 4:
+                    System.out.println("Ứng dụng đã kết thúc.");
+                    break;
+                default:
+                    System.out.println("Lựa chọn không hợp lệ. Vui lòng chọn lại.");
+                    break;
 
-        }while  (choice != 4);
+            }
+        } while (choice != 4);
 
     }
 
 
+    private static void findCustomerByName() {
+        System.out.print("Nhập Người Cần Tìm: ");
+        String findName = sc.nextLine();
+        if (customerMap.containsKey(findName)) {
+            Customer findCustomer = customerMap.get(findName);
+            System.out.println("Tên Khách: " + findCustomer.getName());
+            System.out.println("Email: " + findCustomer.getEmail());
+            System.out.println("Số Điện Thoại: " + findCustomer.getPhone());
+            System.out.println();
+        } else {
+            System.out.println("Không Thể Tìm Thấy:");
+        }
 
-    private void displayAllCustomers() {
-        try  {
+    }
+
+
+    private static void displayAllCustomers() {
+        try {
             for (String i : customerMap.keySet()) {
                 Customer displayCustomer = customerMap.get(i);
                 System.out.println("Tên Khách: " + displayCustomer.getName());
@@ -50,29 +70,29 @@ public class Main {
                 System.out.println("Số Điện Thoại: " + displayCustomer.getPhone());
                 System.out.println();
             }
-    }
-
-    private void findCustomerByName() {
-            System.out.print("Nhập Người Cần Tìm: ");
-            String findName = sc.nextLine();
-            if (customerMap.containsKey(findName)) {
-                Customer findCustomer = customerMap.get(findName);
-                System.out.println("Tên Khách: " + findCustomer.getName());
-                System.out.println("Email: " + findCustomer.getEmail());
-                System.out.println("Số Điện Thoại: " + findCustomer.getPhone());
-                System.out.println();
-            } else {
-                System.out.println("Không Thể Tìm Thấy:");
-            }
+        }catch (Exception e){
 
         }
-
     }
 
-    private void AddCustomer( {
-
+    private static void AddCustomer() {
+        try {
+            System.out.println("Hãy Nhập Thông tin");
+            System.out.print("Nhập Tên Khách: ");
+            String name = sc.nextLine();
+            System.out.print("Nhập Email: ");
+            String email = sc.nextLine();
+            System.out.print("Nhập Số Điện Thoại: ");
+            String phoneNumber = sc.nextLine();
+            System.out.println();
+            Customer customer = new Customer(name, email, phoneNumber);
+            if (customer != null){
+                customerMap.put(name, customer);
+                throw new Exception("Đã Thêm Thành Công!!");
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
-
 
 }
-
